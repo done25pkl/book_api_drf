362a329d422453baaffb67ff42ee6d831cb83926
done25pkl <done1825pkl@gmail.com>
2025-06-10 20:54:05 +0700

    update for generic view

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookListCreateView, BookDetailView
 
-router = DefaultRouter()
-router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
