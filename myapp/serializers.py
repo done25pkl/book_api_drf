edd6426fee424da4f2865cff148ee2814615593d
done25pkl <done1825pkl@gmail.com>
2025-06-12 09:10:56 +0700

    validated data

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Book
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+import re
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,11 +16,37 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
+    username = serializers.CharField(
+        required=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[\w]+$',
+                message='Username can only contain letters, numbers, and underscores.'
+            )
+        ]
+    )
+
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+    def validate_password(self, value):
+        if value.lower() in ['password', '12345678']:
+            raise ValidationError("Password is too common.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise ValidationError("A user with this email already exists.")
+        return value
+
+    def validate(self, data):
+        if data['username'].lower() in data['email'].lower():
+            raise ValidationError("Username should not be part of the email.")
+        return data
 
     def create(self, validated_data):
         user = User.objects.create_user(
